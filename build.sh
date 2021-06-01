98e9f38f562663fb9bee8e735a2177162b87fe2c
Valentine Silvansky <v.silvansky@gmail.com>
2021-06-01 20:25:27 +0300

    Update for OpenSSL 1.1.1i

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-curl -O https://www.openssl.org/source/openssl-1.1.1b.tar.gz
-tar xf openssl-1.1.1b.tar.gz
-cd openssl-1.1.1b
+# Get OpenSSL
+if [[ ! -d openssl ]]
+then
+	git clone git://git.openssl.org/openssl.git
+fi
+
+cd openssl
+
+# Switch to patched tag
+git reset --hard
+git checkout OpenSSL_1_1_1i
+
 ../build_openssl_dist.sh
